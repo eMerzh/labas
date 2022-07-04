6d205fd2e46abf5d41acd1deb59c35a43f5ce8c5
Brice Maron <brice@bmaron.net>
2022-07-04 21:07:43 +0200

    feat: add basic analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { usePostHog } from 'next-use-posthog'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePostHog('phc_f0Uk8a68uDl7LsYOORxU22Z51ifidvXL48hHldpdz3i', {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing();
+    },
+  })
+
   return <Component {...pageProps} />
 }
 
