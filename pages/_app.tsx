af927d6a74ce50d68d083a46a759867ce24fa8a8
Brice Maron <brice@bmaron.net>
2022-07-07 14:31:49 +0200

    chore: add env variable for deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,9 @@ import { usePostHog } from "../lib/usePostHog";
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePostHog("phc_f0Uk8a68uDl7LsYOORxU22Z51ifidvXL48hHldpdz3i", {
-    api_host: "https://app.posthog.com",
+    persistence_name: "labas",
+    secure_cookie: true,
+    cross_subdomain_cookie: false,
     loaded: (posthog) => {
       if (process.env.NODE_ENV === "development") posthog.opt_out_capturing();
     },
